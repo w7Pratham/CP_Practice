f11a33247efbd71a4dc6eac6c18ae542fbf6de63
Prathamesh Shingne <69480155+Prathamesh1710@users.noreply.github.com>
2022-09-03 06:33:38 +0530

    Create leet94_BinaryTreeTraversal.py

---
+++ leet94_BinaryTreeTraversal.py
@@ -0,0 +1,9 @@
+class Solution:
+    def preOrder(self, root):
+        return [root.val] + self.preOrder(root.left) + self.preOrder(root.right) if root else []
+
+    def inOrder(self, root):
+        return self.inOrder(root.left) + [root.val] + self.inOrder(root.right) if root else []
+
+    def postOrder(self, root):
+        return self.postOrder(root.left) + self.postOrder(root.right) + [root.val] if root else []
