c50306e320d5a9be04bd8ec8b1cb44cced2a9103
Prathamesh Shingne <prathamshingne@gmail.com>
2022-10-07 21:02:51 +0530

    Update leet2363.py

--- leet2363.py
+++ leet2363.py
@@ -1,16 +1,14 @@
 class Solution:
     def mergeSimilarItems(self, items1, items2):
         dit = {}
-        for va,wt in items1:
+        item = items1 + items2
+
+        for va,wt in item:
             if va in dit:
                 dit[va] += wt
             else:
                 dit[va] = wt
-        for va,wt in items2:
-            if va in dit:
-                dit[va] += wt
-            else:
-                dit[va] = wt
+
         ls = list(dit.items())
         ls.sort(key = lambda x:x[0])
         return ls
